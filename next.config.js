f329a855adc50a3c6edf622fe0b7dde95e75cec0
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-01-11 20:18:03 +0700

    build: :wrench: add hintrc and update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    };
+    return config;
+  },
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
